e7de6351e3ae191ae80aa630f7cc84869bfd8577
bsaktyas <127457319+bsaktyas@users.noreply.github.com>
2023-12-13 15:35:43 +0700

    modul

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+
+header("Content-Type: application/json; charset=UTF-8");
+
+
+include 'Routes/ProductRoutes.php';
+
+use app\Routes\ProductRoutes;
+
+// get request method
+$method = $_SERVER['REQUEST_METHOD'];
+// get request path
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// call routes
+$productRoute = new ProductRoutes();
+$productRoute->handle($method, $path);
+
